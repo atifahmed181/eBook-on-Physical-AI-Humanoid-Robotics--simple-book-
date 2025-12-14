4a18744275fe089633f81ba2a3f4202b8c1eaab5
atifahmed181 <atifahmed181@gmail.com>
2025-12-14 19:46:31 +0500

    button on main page fixed

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/module1-ros2/">
+            Start reading
           </Link>
         </div>
       </div>
